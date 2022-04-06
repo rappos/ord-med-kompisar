3199bf1e94333c1e01f3d19624bc4b330fee1f36
Emil Rapp <emilemail4@gmail.com>
2022-04-06 11:16:33 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,32 @@
-import slack
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 import os
 from pathlib import Path
+import datetime
+import logging
 from dotenv import load_dotenv
+
+
+logger = logging.getLogger(__name__)
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+client = WebClient(token=os.environ['SLACK_TOKEN'])
 
-client.chat_postMessage(channel='#ord-med-kompisar', text='Testar att skriva')
+channel_id="ord-med-kompisar"
+spel_lista= ["Wordle","Worldle","Globle","Heardle","Dordle","Waffle","Factle"]
+for i in range(1,158):
+    today = datetime.datetime.now() + datetime.timedelta(days=i)
+    scheduled_time = datetime.time(hour=7,minute=0)
+    schedule_timestamp = datetime.datetime.combine(today, scheduled_time).strftime('%s')
+    for m in spel_lista:
+        try:
+            result = client.chat_scheduleMessage(
+                channel=channel_id,
+                text = f"{m}, {today}",
+                post_at=schedule_timestamp
+            )
+            logger.info(result)
+        except SlackAPIError as e:
+            logger.error(e)
